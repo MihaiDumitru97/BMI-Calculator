cd594b3fa2420746b79e1e3ba68e295658078c2d
MihaiDumitru97 <mihai.emil97@gmail.com>
2022-08-25 13:50:15 +0200

    Initial Commit

---
+++ BMI Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Mihai Dumitru on 03/06/1401 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
